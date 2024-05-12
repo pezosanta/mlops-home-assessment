5378822f05d3ddd932be0c1482b59fe31859da09
Santa Peter <pezosanta@gmail.com>
2024-05-12 17:16:54 +0200

    typo fix in comment

--- requirements-prod.sh
+++ requirements-prod.sh
@@ -3,5 +3,5 @@
 # Setting the relative path of current script's parent directory into an environment variable
 CURRENT_SCRIPT_DIR=`dirname ${BASH_SOURCE[0]}`
 
-# Installing development package dependencies
+# Installing production package dependencies
 pip install -r $CURRENT_SCRIPT_DIR/requirements-prod.txt --no-cache-dir
