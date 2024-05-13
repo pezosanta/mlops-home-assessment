6302e7d9ab527242a27439d52324ea73fb36b693
Santa Peter <pezosanta@gmail.com>
2024-05-13 20:05:11 +0200

    added quotes

--- docker/build.sh
+++ docker/build.sh
@@ -13,8 +13,8 @@ source $workSpace/create_build_context.sh
 
 # Building the Docker image
 docker build \
-    --build-arg PYTHON_V=$pythonVersion \
-    -t $dockerImageTag
+    --build-arg PYTHON_V="$pythonVersion" \
+    -t "$dockerImageTag"
 
 # Cleanup the Docker build context
 source $workSpace/cleanup_build_context.sh
