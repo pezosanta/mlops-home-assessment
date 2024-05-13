5883acca0c670aae4a3129e828300b3251124442
Santa Peter <pezosanta@gmail.com>
2024-05-13 19:45:58 +0200

    docker build context

---
+++ docker/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Exit bash whenever an error occurs
+# set -e
+
+workSpace=`dirname ${BASH_SOURCE[0]}`
+
+# Configure variables
+source $workSpace/configure.sh
+
+# Creating the Docker build context
+source $workSpace/create_build_context.sh
+
+# Building the Docker image
+docker build \
+    --build-arg PYTHON_V=$pythonVersion \
+    -t $dockerImageTag
+
+# Cleanup the Docker build context
+source $workSpace/cleanup_build_context.sh
