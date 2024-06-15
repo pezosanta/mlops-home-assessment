a1a63795c2bed72bcc763a29e5a0fa83632b5b42
Peter Santa <pezosanta@gmail.com>
2024-06-15 16:07:27 +0200

    sourcing install-requirements-build.sh in install-requirements-cq.sh

--- scripts/install-requirements-dev.sh
+++ scripts/install-requirements-dev.sh
@@ -3,10 +3,7 @@
 # Setting the relative path of current script's parent directory into an environment variable
 CURRENT_SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 
-# Installing Python package build related packages
-source "$CURRENT_SCRIPT_DIR"/install-requirements-build.sh
-
-# Installing code quality related packages and pre-commit hooks
+# Installing poetry and code quality related packages and pre-commit hooks
 source "$CURRENT_SCRIPT_DIR"/install-requirements-cq.sh
 
 # Installing root (in editable mode) + main, train, dev and prod package dependencies
