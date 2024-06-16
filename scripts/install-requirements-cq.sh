ca037b09f22b9581d35623b518a6c1bf68fb2da7
Peter Santa <pezosanta@gmail.com>
2024-06-16 11:48:44 +0200

    removed install-requirements-build.sh sourcing in install-requirements-cq.sh

--- scripts/install-requirements-cq.sh
+++ scripts/install-requirements-cq.sh
@@ -4,9 +4,6 @@
 CURRENT_SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 PROJECT_DIR="$CURRENT_SCRIPT_DIR"/../
 
-# Installing Python package build related packages
-source "$CURRENT_SCRIPT_DIR"/install-requirements-build.sh
-
 # Installing code quality related packages and pre-commit hooks
 cd "$PROJECT_DIR" || { echo "Could not cd to $PROJECT_DIR"; exit 1; }
 poetry install --only test --no-root
