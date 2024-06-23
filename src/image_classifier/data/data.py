790eb805f126338d9ad422cb14caad29da6b6230
Peter Santa <pezosanta@gmail.com>
2024-06-23 17:48:46 +0200

    removed test loggings

--- src/image_classifier/data/data.py
+++ src/image_classifier/data/data.py
@@ -21,11 +21,6 @@ def get_db_connect_object(db_pass: str, db_config: Path = Path(__file__).parent.
     def mocked_db_connector(db_host: str, db_name: str, db_user: str, db_pass: str) -> object:
         return object
 
-    logger.debug(msg="This is a debug message.")
-    logger.info(msg="This is a info message.")
-    logger.warning(msg="This is a warning message.")
-    logger.error(msg="This is a error message.")
-    logger.critical(msg="This is a critical message.")
     logger.info(msg="Getting the DB connetion object.")
 
     # Getting non-secret db infos from config file
