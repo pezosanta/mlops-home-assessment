975849e11eb8f9c9270a5c6da8e92cacfd6aacdb
Peter Santa <pezosanta@gmail.com>
2024-08-14 10:03:27 +0200

    modify model to better suit FashionMNIST dataset

--- src/image_classifier/modeling/model.py
+++ src/image_classifier/modeling/model.py
@@ -1,57 +1,42 @@
 import logging
-from pathlib import Path
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 logger = logging.getLogger("MODEL")
 logger.setLevel(logging.INFO)
 
 
-class Net(nn.Module):
+class SimpleNet(nn.Module):
     def __init__(self) -> None:
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        super(SimpleNet, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_features=32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+
+        self.fc1 = nn.Linear(in_features=64 * 6 * 6, out_features=600)
+        self.drop = nn.Dropout(0.25)
+        self.fc2 = nn.Linear(in_features=600, out_features=120)
+        self.fc3 = nn.Linear(in_features=120, out_features=10)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
 
-
-def get_model(
-    pretrained_model_path: str | None = None, base_path: Path = Path(__file__).parents[3].joinpath("experiments")
-) -> nn.Module:
-
-    # Handling model structure based hyperparameters here (if there were any)
-    logger.info(msg="Composing and returning model.")
-    model = Net()
-
-    if pretrained_model_path:
-        full_model_path = base_path.joinpath(pretrained_model_path)
-
-        logger.info(msg=f"Loading pretrained model ({str(full_model_path)}).")
-        model.load_state_dict(state_dict=torch.load(full_model_path))
-
-    return model
-
-
-def save_model(
-    model: Net, experiment_name: str, run_name: str, base_path: Path = Path(__file__).parents[3].joinpath("experiments")
-) -> None:
-
-    run_path = base_path.joinpath(f"{experiment_name}/{run_name}")
-    run_path.mkdir(parents=True, exist_ok=True)
-
-    logger.info(msg=f"Saving model to {str(run_path)}.")
-    torch.save(model.state_dict(), run_path.joinpath("model.pth"))
+        return out
